4c664318bb8ec19807509099e31395891bcedf23
htetmoeaung <htetmoeaung760@gmail.com>
2024-07-24 18:37:02 +0630

    Day-2

---
+++ Exercises/Day3_Operators.py
@@ -0,0 +1,83 @@
+# 1
+import math
+
+age = 12
+
+# 2
+height = 168.5
+
+# 3 complex number
+num1 = 1+1j
+
+# 4
+# base = int(input('Enter base: '))
+# height1 = int(input('Enter height: '))
+# area = 0.5*base*height1
+# print('the area of the triangle: ',area)
+
+# 5
+# size_a = int(input('Enter size a: '))
+# size_b = int(input('Enter size b: '))
+# size_c = int(input('Enter size c: '))
+# perimeter = size_a+size_b+size_c
+# print('the perimeter of the: ',perimeter)
+
+# 6
+# length = 5
+# width = 10
+# area = length*width
+# perimeter = 2*(width+length)
+# print(area,perimeter)
+
+# 7
+# pi = 3.14
+# radius = 4
+# area = pi * radius * radius
+# circumference = 2 * pi * radius
+# print(area,circumference)
+
+# 8 slope
+equation = 'y=2x+1'
+slope = 2
+
+# 9 (2,2)(6,10)
+m = (6-2)/(10-2)
+print(m)
+d = math.sqrt(((6-2)**2) + ((10-2)**2))
+print(d)
+
+# 10
+print(slope==m)
+
+# 12
+print(len('python') != len('dragon'))
+
+# 13
+print('on' in 'python','on' in 'dragon')
+
+# 14
+sentence = 'i hope this course is not full of jargon'
+print('jargon' in sentence)
+
+# 16
+num2 = 5
+result = (num2%2) == 0
+
+# 20
+print(int(9.8) == 10)
+
+# 23
+for i in range (1,6):
+    print(i,1,i,i**2,i**3)
+
+
+
+
+
+
+
+
+
+
+
+
--- test.py
+++ test.py
@@ -1,10 +1,17 @@
-import webbrowser
+# import webbrowser
+#
+# def watch_youtube_video(video_url):
+#     webbrowser.open(video_url)
+#
+# # Replace the URL below with the link to the YouTube video you want to watch
+# youtube_video_url = "https://www.youtube.com/watch?v=B9synWjqBn8"
+#
+# if __name__ == "__main__":
+#     watch_youtube_video(youtube_video_url)
 
-def watch_youtube_video(video_url):
-    webbrowser.open(video_url)
 
-# Replace the URL below with the link to the YouTube video you want to watch
-youtube_video_url = "https://www.youtube.com/watch?v=B9synWjqBn8"
+# Complex numbers
+print('Complex number: ', 1 + 1j)
+print('Multiplying complex numbers: ',(1 + 1j) * (1 - 1j))
 
-if __name__ == "__main__":
-    watch_youtube_video(youtube_video_url)
+print('on' in 'Python', 'on' in 'dragon')
