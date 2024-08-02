ef13a4e4daa51d853219de1e64e78dd16a10c34e
htetmoeaung <htetmoeaung760@gmail.com>
2024-08-02 21:05:26 +0630

    Before 8.2024

--- Problems/two_sums.py
+++ Problems/two_sums.py
@@ -1,9 +1,9 @@
 numbers = [1,2,5,7,9]
 target = 8
 
-n = len(numbers)
-for i in numbers:
-    for j in numbers:
-        if (i + j) == target:
-            print(i,j)
-        break
+# n = len(numbers)
+# for i in numbers:
+#     for j in numbers:
+#         if (i + j) == target:
+#             print(i,j)
+#         break
