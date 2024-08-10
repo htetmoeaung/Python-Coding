8bb00a92fb285ccae6a9fec642fe0e016d81619d
htetmoeaung <htetmoeaung760@gmail.com>
2024-08-10 11:03:03 +0630

    10.8.2024

--- Lessons/3List and Sets/Lesson18_Index_Operators.py
+++ Lessons/3List and Sets/Lesson18_Index_Operators.py
@@ -1,2 +1,19 @@
-# Index Operators [] = give access to a sequance's element (str,list,tuples)
+# Index Operators [] = give access to a sequence's element
+#                      (str,list,tuples)
+
+name = 'htet moe?'
+
+if(name[0].lower()):
+    name = name.capitalize()
+print(name)
+
+first_name = name[0:4].upper()
+last_name = name[5:].upper()
+last_char = name[-1]
+
+print(first_name)
+print(last_name)
+print(last_char)
+
+
 
