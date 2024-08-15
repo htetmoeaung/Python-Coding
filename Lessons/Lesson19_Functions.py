c37887827e22d2af471f3db2deb6113d9d018472
htetmoeaung <htetmoeaung760@gmail.com>
2024-08-15 17:24:00 +0630

    10.8.2024

---
+++ Lessons/Lesson19_Functions.py
@@ -0,0 +1,10 @@
+# Function = a block of code which is executed only when it is called
+
+def hello(first_name, last_name, age):
+    print('Hello', first_name + ' ' + last_name)
+    print('You are', age, 'years old.')
+
+
+hello('htet', 'moe', 20)
+
+print()
