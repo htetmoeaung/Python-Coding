e1e1befd1595525254d0ce0f561302605e9dd912
htetmoeaung <htetmoeaung760@gmail.com>
2024-08-19 11:00:44 +0630

    18.8.2024

---
+++ Lessons/Lesson28_Exception.py
@@ -0,0 +1,20 @@
+# Exception = events detected during  execution
+#             that interrupt the flow of a program
+
+try:
+    num1 = int(input('Enter a number to divide: '))
+    num2 = int(input('Enter a number to dicide by: '))
+    result = num1 / num2
+except ZeroDivisionError:
+    print("You can't divided the number by ZERO")
+except ValueError as e:
+    print('You can only put NUMBERS')
+    print(e)
+except Exception:
+    print('Something went wrong!!')
+else:
+    print(result)
+finally:
+    print('This is always execute')
+
+
---
+++ Lessons/Lesson29_FileDetection.py
@@ -0,0 +1,12 @@
+import os
+
+path = 'C:\\Users\\user\\Desktop\\Resources\\test.txt'
+
+if os.path.exists(path):
+    print('That location exist')
+    if os.path.isfile(path):
+        print('This is a File')
+    elif os.path.isdir(path):
+        print('This is a Folder or Directory')
+else:
+    print("That location doesn't exist")
