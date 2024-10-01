47a33218850a33e560a2e35efcca1edb678c5893
htetmoeaung <htetmoeaung760@gmail.com>
2024-10-01 13:45:53 +0630

    22.9.2024

---
+++ Lessons/Lesson53_List_Comprehension.py
@@ -0,0 +1,14 @@
+# List Comprehension = a way to create new list with less syntax
+#                     can mimic certain lambda functions, easier to read
+#                     list = [expression for item in iterable]
+#                     list = [expression for item in iterable if conditional]
+#                     list = [expression if/else for item in iterable]
+
+students = [100,90,81,70,64,50,43,33,0]
+
+
+
+
+
+
+
